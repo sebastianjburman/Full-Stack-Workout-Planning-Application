09c95da5588539a2a78da10eba66a7521849f48e
Sebastian Burman <74584954+sebastianjburman@users.noreply.github.com>
2023-01-01 17:07:40 -0500

    Fixed password error on frontend and backend

--- Backend/DTO/UserDTO.cs
+++ Backend/DTO/UserDTO.cs
@@ -11,7 +11,7 @@ namespace Backend.DTO
         [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$", ErrorMessage = "Email is not in correct format.")]
         public string? Email { get; set; }
         [Required]
-        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$", ErrorMessage = "Password be minimum 8 and maximum 20 characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,20}$", ErrorMessage = "Password be minimum 8 and maximum 20 characters, at least one uppercase letter, one lowercase letter, and one number.")]
         [BsonElement("password")]
         public string? Password { get; set; }
         [Required]
